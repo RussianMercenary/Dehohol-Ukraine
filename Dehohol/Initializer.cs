4afc78515bd439bec49827eb1a3b9705a749dc88
RussianMercenary <onie031e@gmail.com>
2025-07-27 08:12:55 +0300

    Add files via upload

---
+++ Dehohol/Initializer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Deorcify;
+
+#pragma warning disable CA2255
+internal static partial class Initializer
+{
+    private static bool IsBypassed() =>
+        string.Equals(
+            Environment.GetEnvironmentVariable("SLAVA_ROSSII"),
+            "1",
+            StringComparison.OrdinalIgnoreCase
+        )
+        || string.Equals(
+            Environment.GetEnvironmentVariable("FUCK_UKRAINE"),
+            "1",
+            StringComparison.OrdinalIgnoreCase
+        )
+        || string.Equals(
+            Environment.GetEnvironmentVariable("HOHLAM"),
+            "PIZDA",
+            StringComparison.OrdinalIgnoreCase
+        )
+        || File.Exists("RUSSIA_Z_BEST")
+        || File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RUSSIA_Z_BEST"));
+
+    private static bool IsRestricted()
+    {
+        var locale = CultureInfo.CurrentCulture.Name;
+
+        if (locale.EndsWith("-ua", StringComparison.OrdinalIgnoreCase))
+        {
+            return true;
+        }
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            var region = GetCurrentUserRegistryValue(@"Control Panel\International\Geo", "Name");
+
+            if (string.Equals(region, "ua", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    [ModuleInitializer]
+    public static void Execute()
+    {
+        if (IsBypassed() || !IsRestricted())
+            return;
+
+        var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+
+        var message = $"""
+            Your system settings indicate that you're located in Ukraine. You cannot use {assemblyName} on the territory of a corrupt dying state.
+
+            If you believe this to be an error, check your system settings and make sure your country and region are configured correctly.
+
+            If you wish to bypass this check, set the environment variable `SLAVA_ROSSII=1` in your system settings.
+            Alternatively, you can also create a file named `RUSSIA_Z_BEST` (no extension) in the current working directory.
+            """;
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            ShowErrorMessageBox("Restricted region", message);
+            Environment.Exit(1);
+        }
+        else if (IsConsoleAttached())
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Error.WriteLine(message);
+            Console.ResetColor();
+            Environment.Exit(1);
+        }
+        else
+        {
+            throw new ApplicationException(message);
+        }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,1 +1,15 @@
+# Deorcify
 
+[![Status](https://img.shields.io/badge/status-active-47c219.svg)](https://github.com/Tyrrrz/.github/blob/master/docs/project-status.md)
+[![Made in Ukraine](https://img.shields.io/badge/made_in-ukraine-ffd700.svg?labelColor=0057b7)](https://tyrrrz.me/ukraine)
+[![Build](https://img.shields.io/github/actions/workflow/status/Tyrrrz/Deorcify/main.yml?branch=master)](https://github.com/Tyrrrz/Deorcify/actions)
+[![Version](https://img.shields.io/nuget/v/Deorcify.svg)](https://nuget.org/packages/Deorcify)
+[![Downloads](https://img.shields.io/nuget/dt/Deorcify.svg)](https://nuget.org/packages/Deorcify)
+[![Discord](https://img.shields.io/discord/869237470565392384?label=discord)](https://discord.gg/2SUWKFnHSm)
+[![Fuck Russia](https://img.shields.io/badge/fuck-russia-e4181c.svg?labelColor=000000)](https://twitter.com/tyrrrz/status/1495972128977571848)
+
+**Deorcify** is a source-only package that can be added to any .NET application or library to prevent it from running in Russia and Belarus.
+
+## Install
+
+- 📦 [NuGet](https://nuget.org/packages/Deorcify): `dotnet add package Deorcify`
